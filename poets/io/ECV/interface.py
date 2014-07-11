c19b4b167af9ffcd7a36e59cbc853acf578fcb05
tmistelbauer <t.mistelbauer@gmail.com>
2014-07-11 10:16:36 +0200

    added temporal resolution

--- poets/io/ECV/interface.py
+++ poets/io/ECV/interface.py
@@ -50,50 +50,50 @@ class ECV(BasicSource):
     """
 
     def __init__(self, **kwargs):
-        
+
         name = 'ECV'
         source_path = "ftp.ipf.tuwien.ac.at/_down/daily_files/COMBINED"
         dirstruct = ['YYYY']
         filename = "ESACCI-SOILMOISTURE-L3S-SSMV-COMBINED-{YYYY}{MM}{TT}{hh}{mm}{ss}-fv02.0.nc"
-
+        temp_res = 'daily'
         begin_date = datetime.date(1978, 11, 01)
         variables = ['sm']
 
         if source_path[-1] != '/':
             source_path += '/'
 
-        super(ECV, self).__init__(name, source_path, filename, dirstruct,
-                                     begin_date, variables)
+        super(ECV, self).__init__(name, source_path, filename, temp_res,
+                                  dirstruct, begin_date, variables)
 
     def download(self, download_path=None, begin=None, end=None):
-        
+
         # download to Settings.tmp_path
-        
+
         if download_path == None:
             download_path = os.path.join(Settings.tmp_path, self.name)
-        
+
         if begin == None:
             begin = self.begin_date
-            
+
         if end == None:
             end = datetime.datetime.now()
-        
-        
+
+
         print('[INFO] downloading data from ' + str(begin) + ' - '
               + str(end) + '-12-31')
-        
+
         remotepath = '/_down/daily_files/COMBINED/'
-        localpath = download_path 
-        
+        localpath = download_path
+
         host = 'ftp.ipf.tuwien.ac.at'
         port = 22
         transport = paramiko.Transport((host, port))
         password = "FRT$_22"  # hard-coded
         username = "ecv_sm_v2"  # hard-coded
         transport.connect(username=username, password=password)
-        
+
         sftp = paramiko.SFTPClient.from_transport(transport)
-   
+
         for year in sftp.listdir(remotepath):
             # if not os.path.exists(localpath + str(year)):
                 # os.makedirs(localpath + str(year))
@@ -104,7 +104,7 @@ class ECV(BasicSource):
                     sftp.get(remotepath + str(year) + '/' + filename,
                              localpath + '/' + filename)
                     sftp.close
-                    
+
             if int(year) == end:
                 break
 
