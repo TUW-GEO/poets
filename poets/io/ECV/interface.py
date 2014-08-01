7c0b4120db6775baaf5e9d4decd11cb1e5778019
tmistelbauer <t.mistelbauer@gmail.com>
2014-08-01 17:08:02 +0200

    updated ECV source class

--- poets/io/ECV/interface.py
+++ poets/io/ECV/interface.py
@@ -18,33 +18,68 @@
 # Creation date: 2014-07-09
 
 import datetime
-import os
-from poets.settings import Settings
 from poets.io.source_base import BasicSource
-import paramiko
 
 
 class ECV(BasicSource):
 
     """Source Class for ECV data.
 
-    Attributes
+    Parameters
     ----------
     name : str
         Name of the data source
-    source_path : str
-        Link to data source
-    begin_date : datetime.date
-        Date, from which on data is available
     filename : str
         Structure/convention of the file name
     filedate : dict
         Position of date fields in filename, given as tuple
     temp_res : str
         Temporal resolution of the source
+    host : str
+        Link to data host
+    protocol : str
+        Protocol for data transfer
+    username : str, optional
+        Username for data access
+    password : str, optional
+        Password for data access
+    port : int, optional
+        Port to data host, defaults to 22
+    directory : str, optional
+        Path to data on host
     dirstruct : list of strings
-        Structure of source directory
-        Each list item represents a subdirectory
+        Structure of source directory, each list item represents a subdirectory
+    begin_date : datetime.date, optional
+        Date from which on data is available, defaults to 2000-01-01
+    variables : list of strings, optional
+        Variables used from data source
+
+    Attributes
+    ----------
+    name : str
+        Name of the data source
+    filename : str
+        Structure/convention of the file name
+    filedate : dict
+        Position of date fields in filename, given as tuple
+    temp_res : str
+        Temporal resolution of the source
+    host : str
+        Link to data host
+    protocol : str
+        Protocol for data transfer
+    username : str
+        Username for data access
+    password : str
+        Password for data access
+    port : int
+        Port to data host
+    directory : str
+        Path to data on host
+    dirstruct : list of strings
+        Structure of source directory, each list item represents a subdirectory
+    begin_date : datetime.date
+        Date from which on data is available
     variables : list of strings
         Variables used from data source
     """
@@ -52,100 +87,25 @@ class ECV(BasicSource):
     def __init__(self, **kwargs):
 
         name = 'ECV'
-        source_path = "ftp.ipf.tuwien.ac.at/_down/daily_files/COMBINED"
-        source_type = 'SFTP'
-        dirstruct = ['YYYY']
         filename = ("ESACCI-SOILMOISTURE-L3S-SSMV-COMBINED-{YYYY}{MM}{TT}{hh}"
                     "{mm}{ss}-fv02.0.nc")
         filedate = {'YYYY': (38, 42), 'MM': (42, 44), 'DD': (44, 46),
                     'hh': (46, 48), 'mm': (48, 50), 'ss': (50, 52)}
         temp_res = 'daily'
+        host = "ftp.ipf.tuwien.ac.at/"
+        protocol = 'SFTP'
+        username = 'ecv_sm_v2'
+        password = 'FRT$_22'
+        port = 22
+        directory = "_down/daily_files/COMBINED/"
+        dirstruct = ['YYYY']
         begin_date = datetime.datetime(1978, 11, 01)
         variables = ['sm']
 
-        if source_path[-1] != '/':
-            source_path += '/'
+        super(ECV, self).__init__(name, filename, filedate, temp_res, host,
+                                  protocol, username, password, port,
+                                  directory, dirstruct, begin_date, variables)
 
-        super(ECV, self).__init__(name, source_path, source_type, filename,
-                                  filedate, temp_res, dirstruct, begin_date,
-                                  variables)
-
-#==============================================================================
-#     def download(self, download_path=None, begin=None, end=None):
-#         """Download latest ECV dekadal data
-#
-#         Parameters
-#         ----------
-#         download_path : str, optional
-#             Path where to save the downloaded files.
-#         begin : datetime.date, optional
-#             Set either to first date of remote repository or date of
-#             last file in local repository
-#         end : datetime.date, optional
-#             Entered in [years]. End year is not downloaded anymore.
-#             Set to today if none given
-#
-#         Returns
-#         -------
-#         bool
-#             true if data is available, false if not
-#         """
-#
-#         if download_path == None:
-#             download_path = os.path.join(Settings.tmp_path, self.name)
-#
-#         if not os.path.exists(download_path):
-#             print('[INFO] output path does not exist... creating path')
-#             os.makedirs(download_path)
-#
-#         if begin == None:
-#             begin = self._get_download_date()
-#
-#         if end == None:
-#             end = datetime.datetime.now()
-#
-#         print('[INFO] downloading data from ' + str(begin) + ' - '
-#               + str(end) + '-12-31')
-#
-#         remotepath = '/_down/daily_files/COMBINED/'
-#         localpath = download_path
-#
-# connect to ftp server
-#         host = 'ftp.ipf.tuwien.ac.at'
-#         port = 22
-#         transport = paramiko.Transport((host, port))
-# password = "FRT$_22"  # hard-coded
-# username = "ecv_sm_v2"  # hard-coded
-#         transport.connect(username=username, password=password)
-#
-#         sftp = paramiko.SFTPClient.from_transport(transport)
-#
-#         subdirs = sftp.listdir(remotepath)
-#
-# check if data is available
-#         if str(begin.year) not in subdirs:
-#             return False
-#
-# download files to temporary path
-#         for year in subdirs:
-#             if str(begin.year) > year:
-#                 continue
-#             if str(end.year) < year:
-#                 continue
-#             files = sftp.listdir(os.path.join(remotepath, str(year)))
-#             files.sort()
-#             for filename in files:
-#                 fdate = self.get_file_date(filename)
-#                 if fdate < begin:
-#                     continue
-#                 if fdate > end:
-#                     continue
-#                 if os.path.isfile(os.path.join(localpath, filename)) is False:
-#                     sftp.get(remotepath + str(year) + '/' + filename,
-#                              os.path.join(localpath, filename))
-#                     sftp.close
-#         return True
-#==============================================================================
 
 if __name__ == "__main__":
     pass
