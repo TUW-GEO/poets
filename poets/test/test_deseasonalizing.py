6493d933fefcd9fe2a29d376dc49f19f4777c681
ipfeil <isy.pfeil@gmx.at>
2014-08-18 10:41:02 +0200

    bugfix

--- poets/test/test_deseasonalizing.py
+++ poets/test/test_deseasonalizing.py
@@ -1,15 +1,28 @@
-'''
-Created on Jul 18, 2014
+# Copyright (c) 2014, Vienna University of Technology (TU Wien), Department
+# of Geodesy and Geoinformation (GEO).
+# All rights reserved.
 
-@author: pydev
-'''
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL VIENNA UNIVERSITY OF TECHNOLOGY,
+# DEPARTMENT OF GEODESY AND GEOINFORMATION BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Author: Isabella Pfeil, isy.pfeil@gmx.at
+# Creation date: 2014-08-18
+
 import unittest
-from poets.time_series.filtering import group_months, group_seasons, ctrd_mov_avg
-from poets.time_series.deseasonalizing import seasonal_indices
-from poets.time_series.deseasonalizing import zscore
+import os
 import pandas as pd
 from datetime import datetime
-import os
+from poets.time_series.filtering import group_months, group_seasons, ctrd_mov_avg
+from poets.time_series.deseasonalizing import seasonal_indices, zscore
 
 
 def curpath():
@@ -33,8 +46,8 @@ class Test(unittest.TestCase):
         pass
 
     def test_group_months(self):
-        len_ts_mon = 396
-        lenDates = 133
+        len_ts_mon = 132
+        lenDates = 45
 
         ts_mon = len(group_months(self.ts, self.attribute)[0])
         ts_dat = len(group_months(self.ts, self.attribute)[1])
@@ -43,8 +56,8 @@ class Test(unittest.TestCase):
         assert lenDates == ts_dat
 
     def test_group_seasons(self):
-        len_ts_seas = 133
-        lenDates = 133
+        len_ts_seas = 45
+        lenDates = 45
 
         ts_months, dates = group_months(self.ts, self.attribute)
 
@@ -55,7 +68,7 @@ class Test(unittest.TestCase):
         assert lenDates == ts_dat
 
     def test_ctrd_mov_avg(self):
-        len_ts_ctrd = 128
+        len_ts_ctrd = 40
         ts_months, dates = group_months(self.ts, self.attribute)
         ts_seas, _ = group_seasons(ts_months, dates)
         ts_ctrd = len(ctrd_mov_avg(ts_seas))
@@ -63,14 +76,14 @@ class Test(unittest.TestCase):
         assert len_ts_ctrd == ts_ctrd
 
     def test_seasonal_indices(self):
-        len_ts_des = 133
+        len_ts_des = 45
 
         ts_des = len(seasonal_indices(self.ts, self.attribute))
 
         assert len_ts_des == ts_des
 
     def test_zscore(self):
-        len_ts_des = 133
+        len_ts_des = 45
 
         ts_des = len(zscore(self.ts, self.attribute))
 
