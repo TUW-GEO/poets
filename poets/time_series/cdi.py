e2dae0923ee432f36a9de652b0a466897bd55488
tmistelbauer <t.mistelbauer@gmail.com>
2014-08-22 14:03:15 +0200

    moved cdi module into time_series package

--- poets/time_series/cdi.py
+++ poets/time_series/cdi.py
@@ -2,6 +2,21 @@
 # of Geodesy and Geoinformation (GEO).
 # All rights reserved.
 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+#
+# * Neither the name of the Vienna University of Technology - Department of
+#   Geodesy and Geoinformation nor the names of its contributors may be used to
+#   endorse or promote products derived from this software without specific
+#   prior written permission.
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -59,7 +74,7 @@ def calc_DI(data, parameter, interest_period=[6, 12, 24], scale_zero=False):
         thematical parameter (precipitation, temperature, vegetation,
         soil moisture)
     interest_period : list of int, optional
-        interest periods used to calculate drought index, 
+        interest periods used to calculate drought index,
         defaults to [6, 12, 24]
     """
 
